60c6212ab4ed6ad358299ba0486c1fa2ee45ce09
dushan-herath <dushan.herath123@icloud.com>
2025-12-14 14:25:07 -0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -41,7 +41,7 @@ class IrradianceForecastDataset(Dataset):
 
         #self.feature_cols = feature_cols or ["ghi", "dni", "dhi", "temp", "pressure"]
         #self.feature_cols = feature_cols or ["ghi", "dni", "dhi", "delta_ghi"]
-        self.feature_cols = feature_cols or ["dni"]
+        self.feature_cols = feature_cols or ["dhi"]
         self.target_cols = target_cols or ["ghi"]
 
         self.sky_col = "raw_image_path"
